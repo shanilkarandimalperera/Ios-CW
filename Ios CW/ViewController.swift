5ccc34d92e8db32a47e158fde0d564290dd833b5
B.S. Randimal Perera <randimalperera62@gmail.com>
2023-04-17 11:42:15 -0700

    Main

---
+++ Ios CW/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ios CW
+//
+//  Created by shanilka on 4/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
